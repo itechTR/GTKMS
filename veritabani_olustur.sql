cc4894f9e9cbd6d3fb7f51258937142cd758027a
iTech <aytekin.kahraman@gmail.com>
2019-04-09 09:27:27 +0300

    Add files via upload

---
+++ veritabani_olustur.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `gtkmsdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `gtkmsdb`;
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` char(13) NOT NULL,
+  `message` mediumtext NOT NULL,
+  `viewed` tinyint(1) NOT NULL,
+  `timestamp` text,
+  `ipaddress` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
